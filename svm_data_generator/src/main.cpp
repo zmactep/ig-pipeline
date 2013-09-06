2dd34c6672bb665826f5af61b1ef44a79d3f2017
Konstantin Fedorov <k.a.feodorov@gmail.com>
2013-09-06 10:09:57 +0400

    bugfixing

--- svm_data_generator/src/main.cpp
+++ svm_data_generator/src/main.cpp
@@ -10,22 +10,84 @@
 #include "nomenclature.h"
 #include "read.h"
 
+std::map<std::string, int> regionName2code;
+
 void usage(){
 	std::cout << "Usage: svm_data_generator fasta_file nomenclature_file sliding_window_size" << std::endl;
 }
 
-void splitReadByNomenclature(const std::string& r, const Nomenclature& n, std::map<std::string, std::string>& output) {
+void split_read_by_nomenclature(const std::string& r, const Nomenclature& n, std::map<int, std::string>& output) {
 	//no length checks here are explicit - we better through an exception
 	int offset = 0;
-	output.insert(std::make_pair("FR1", r.substr(0, n.getCDR1begin() - offset + 1)));
-	offset += output["FR1"].length();
-	output.insert(std::make_pair("CDR1", r.substr(n.getCDR1begin(), n.getFR2begin() - offset + 1)));
-	offset += output["CDR1"].length();
-	output.insert(std::make_pair("FR2", r.substr(n.getFR2begin(), n.getCDR2begin() - offset + 1)));
-	offset += output["FR2"].length();
-	output.insert(std::make_pair("CDR2", r.substr(n.getCDR2begin(), n.getFR3begin() - offset + 1)));
-	offset += output["CDR2"].length();
-	output.insert(std::make_pair("FR3", r.substr(n.getFR3begin(), n.getFR3end() - offset + 1)));
+	std::string region = r.substr(0, n.getCDR1begin() - 1);
+	output.insert(std::make_pair(regionName2code["FR1"], region));
+	offset += region.length();
+
+	region = r.substr(n.getCDR1begin() - 1, n.getFR2begin() - 1 - offset);
+	output.insert(std::make_pair(regionName2code["CDR1"], region));
+	offset += region.length();
+
+	region = r.substr(n.getFR2begin() - 1, n.getCDR2begin() - 1 - offset);
+	output.insert(std::make_pair(regionName2code["FR2"], region));
+	offset += region.length();
+
+	region = r.substr(n.getCDR2begin() - 1, n.getFR3begin() - 1 - offset);
+	output.insert(std::make_pair(regionName2code["CDR2"], region));
+	offset += region.length();
+
+	region = r.substr(n.getFR3begin() - 1, n.getFR3end() - offset);
+	output.insert(std::make_pair(regionName2code["FR3"], region));
+}
+
+void print_svm_entry(int label, const std::string& line, const std::string& comment) {
+	std::cout << label << " ";
+	for (int i = 0; i < line.size(); ++i) {
+		std::cout << i << ":" << (int)line[i] << " ";
+	}
+	std::cout << "# " << comment << std::endl;
+}
+
+void get_seq2nomenclature(const char* filename, std::map<std::string, Nomenclature>& seq2nomenclature) {
+	KabatReader kabat_reader(filename);
+	Nomenclature n;
+	while (!kabat_reader.eof()) {
+		try {
+			kabat_reader >> n;
+			seq2nomenclature.insert(std::make_pair(n.getRefName(), n));
+		} catch (std::exception& e) {
+			std::clog << "Error parsing line in kabat_reader: " << e.what() << std::endl;
+		}
+	}
+}
+
+void process_read(const Read& r, const std::map<std::string, Nomenclature>& seq2nomenclature, int window_size) {
+	std::map<int, std::string> regions;
+	try {
+		split_read_by_nomenclature(r.getSeq(), seq2nomenclature.find(r.getName())->second, regions);
+	} catch (std::exception& e){
+		std::clog << "Error splitting read by nomenclature: " << r.getName() << std::endl;
+	}
+
+	for (std::map<int, std::string>::const_iterator it1 = regions.begin(); it1 != regions.end(); ++it1) {
+		const int label = it1->first;
+
+		if (r.getSeq().length() > window_size) {
+			std::vector<std::string> kmers = KmerGenerator::getKmers(it1->second, window_size);
+			for (std::vector<std::string>::const_iterator it2 = kmers.begin(); it2 != kmers.end(); ++it2) {
+				print_svm_entry(label, *it2, r.getName());
+			}
+		}
+	}
+}
+
+void build_regionName2code() {
+	//SVM uses numerics (not categories) as labels -> we need to provide appropriate mapping
+	regionName2code.insert(std::make_pair("FR1", 1));
+	regionName2code.insert(std::make_pair("CDR1", 2));
+	regionName2code.insert(std::make_pair("FR2", 3));
+	regionName2code.insert(std::make_pair("CDR2", 4));
+	regionName2code.insert(std::make_pair("FR3", 5));
+	regionName2code.insert(std::make_pair("CDR3", 6));
 }
 
 int main(int argc, char ** argv) {
@@ -35,38 +97,17 @@ int main(int argc, char ** argv) {
 	}
 
 	const int window_size = atoi(argv[3]);
+	build_regionName2code();
 
 	try {
-		FastaReader fasta_reader(argv[1]);
-		KabatReader kabat_reader(argv[2]);
-
 		std::map<std::string, Nomenclature> seq2nomenclature;
-		Nomenclature n;
-		while (!kabat_reader.eof()) {
-			kabat_reader >> n;
-			seq2nomenclature.insert(std::make_pair(n.getRefName(), n));
-		}
+		get_seq2nomenclature(argv[2], seq2nomenclature);
 
+		FastaReader fasta_reader(argv[1]);
 		Read r;
 		while (!fasta_reader.eof()) {
 			fasta_reader >> r;
-			std::map<std::string, std::string> regions;
-			try {
-				splitReadByNomenclature(r.getSeq(), n, regions);
-			} catch (std::exception& e){
-				std::clog << "Error processing read: " << r.getName() << std::endl;
-			}
-
-			for (std::map<std::string, std::string>::const_iterator it1 = regions.begin(); it1 != regions.end(); ++it1) {
-				const std::string& label = it1->first;
-
-				std::vector<std::string> kmers = KmerGenerator::getKmers(it1->second, window_size);
-
-				for (std::vector<std::string>::const_iterator it2 = kmers.begin(); it2 != kmers.end(); ++it2) {
-					//TODO output in libsvm format
-					std::cout << label << ":" << *it2 << std::endl;
-				}
-			}
+			process_read(r, seq2nomenclature, window_size);
 		}
 
 	} catch (std::exception& e){
