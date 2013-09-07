b0fcd4344052c34461bb6e3c5a563ebbe9cf3403
Konstantin Fedorov <k.a.feodorov@gmail.com>
2013-09-07 14:58:03 +0400

    small script fix

--- svm_data_generator/compile.sh
+++ svm_data_generator/compile.sh
@@ -10,6 +10,7 @@ echo "Building svm_data_generator"
 cd ../svm_data_generator
 mkdir ./bin
 mkdir ./build
+cd build
 cmake ../src/ -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug
 make
 make install
