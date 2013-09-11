41b98cd49d3a7a6f9d325df4b0bb510c4c14c7a6
Konstantin Fedorov <k.a.feodorov@gmail.com>
2013-09-11 09:26:10 +0400

    merge small regions euristics added

--- parse_svm_output.py
+++ parse_svm_output.py
@@ -8,6 +8,7 @@ def main(argv):
     input_file = argv[1]
     sliding_window_size = int(argv[2])
     svm_window_size = int(argv[3])
+    merge_threshold = int(argv[4])
     svm_head_offset = svm_window_size // 2
     svm_tail_offset = svm_window_size - svm_head_offset
 
@@ -22,7 +23,7 @@ def main(argv):
             averaged_prediction = average_prediction(prediction, sliding_window_size) # list
             debug_prediction.write("%s\n" % (key + '\t' + ''.join(prediction)))
             debug_prediction_avg.write("%s\n" % (key + '\t' + ''.join(averaged_prediction)))
-            results.write("%s\n" % kabat_range(key, averaged_prediction, svm_head_offset, svm_tail_offset))
+            results.write("%s\n" % kabat_range(key, averaged_prediction, svm_head_offset, svm_tail_offset, merge_threshold))
     debug_prediction.close()
     debug_prediction_avg.close()
     results.close()
@@ -39,19 +40,35 @@ def slide_window(iterable, window_size):
     shiftedStarts = [islice(iterable, s, None) for s in range(window_size)]
     return zip(*shiftedStarts)
 
-def kabat_range(name, prediction, svm_head_offset, svm_tail_offset):
+def kabat_range(name, prediction, svm_head_offset, svm_tail_offset, merge_threshold):
     array = []
     offset = 0
     for key, group in groupby(prediction):
-        range = sum([1 for x in group])
+        sum_range = sum([1 for x in group])
         start = offset + 1
-        stop = start + range - 1
+        stop = start + sum_range - 1
         array += start, stop
         offset = stop
     result = [x + svm_head_offset for x in array]
     result[0] = 1
     result[-1] += svm_tail_offset
-    return name + '\t' + '\t'.join([str(x) for x in result])
+
+    #merge regions less than merge_threshold
+    # ex merge_threshold = 7:
+    #[(1, 2), (3, 5), (6, 20)] -> [(1, 20)]
+    #[(10, 20), (30, 50)] -> [(10, 20), (30, 50)]
+    current = (result[0], result[1])
+    result.pop(0)
+    result.pop(0)
+    merged_ranges = []
+    for x, y in [(result[i], result[i + 1]) for i in range(0, len(result), 2)]:
+        if current[1] - current[0] + 1 < merge_threshold: #
+            current = current[0], y
+        else:
+            merged_ranges.append(current)
+            current = (x, y)
+    merged_ranges.append(current)
+    return name + '\t' + '\t'.join([str(x) + '\t' + str(y) for x, y in merged_ranges])
 
 if __name__ == "__main__":
    main(sys.argv)
--- svm_find_regions.sh
+++ svm_find_regions.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-EXPECTED_ARGS=5
+EXPECTED_ARGS=6
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: svm_find_regions.sh input_train.fasta input_kabat input_predict.fasta svm_window_size avg_window_size"
+  echo "Usage: svm_find_regions.sh input_train.fasta input_kabat input_predict.fasta svm_window_size avg_window_size merge_threshold"
   exit
 fi
 
@@ -25,7 +25,7 @@ cd ../../..
 echo "Done"
 
 paste ./svm_data_generator/bin/read_names.txt ./common_lib/third_party/libsvm-string-3.17/output.txt > data.txt
-python ./parse_svm_output.py data.txt $5 $4
+python ./parse_svm_output.py data.txt $5 $4 $6
 
-rm data.txt ./common_lib/third_party/libsvm-string-3.17/traindata.svm.model ./common_lib/third_party/libsvm-string-3.17/output.txt ./svm_data_generator/bin/read_names.txt
+#rm data.txt ./common_lib/third_party/libsvm-string-3.17/traindata.svm.model ./common_lib/third_party/libsvm-string-3.17/output.txt ./svm_data_generator/bin/read_names.txt
 echo "Done. Result is in results.txt. Debug output is in debug_prediction.txt and debug_prediction_avg.txt"
