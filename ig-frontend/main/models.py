62c3dd6a7ba7e9122934492097af9646f5966b9d
Konstantin Fedorov <k.a.feodorov@gmail.com>
2013-10-13 18:09:31 +0400

    duplicates removed

--- ig-frontend/main/models.py
+++ ig-frontend/main/models.py
@@ -1,27 +1,2 @@
-from django.db import models
-from django.core import serializers
-from django.forms import ModelForm
-
-import json
 
 # Create your models here.
-
-class TaskRequest(models.Model):
-    FIND_PATTERNS = 1
-    GENERATE_MODEL = 2
-    MODEL_LIST = 3
-    TASK_CHOICES = (
-        (FIND_PATTERNS, 'find patterns'),
-        (GENERATE_MODEL, 'generate model'),
-        (MODEL_LIST, 'model list')
-    )
-    task = models.IntegerField(choices=TASK_CHOICES, default=MODEL_LIST)
-
-    def __str__(self):
-        data = self.get_queryset()[:1]
-        return json.dumps(serializers.serialize('json', data))
-
-class TaskRequestForm(ModelForm):
-    class Meta:
-        model = TaskRequest
-        fields = ['task']
