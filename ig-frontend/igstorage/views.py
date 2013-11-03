7fe59eb87fcf81b8e8a997ada22f9608064270d1
Konstantin Fedorov <k.a.feodorov@gmail.com>
2013-11-03 15:35:59 +0400

    Issue #16 - StorageItem file_id and comment fields can be updated in interface

--- ig-frontend/igstorage/views.py
+++ ig-frontend/igstorage/views.py
@@ -11,9 +11,22 @@ log = logging.getLogger('all')
 def view(request):
     if request.method == 'POST':  # If the form has been submitted...
         if 'btn_delete' in request.POST:
-            item_id = request.POST.get('id', 'NA')
-            item = StorageItem.objects.using('ig').get(file_id=item_id)
+            file_id = request.POST.get('file_id', 'NA')
+            item = StorageItem.objects.using('ig').get(file_id=file_id)
             item.delete(using='ig')
+            form = StorageItemForm()
+        elif 'btn_update' in request.POST:
+            file_id = request.POST.get('file_id', 'NA')
+            new_file_id = request.POST.get('modified_id', 'NA')
+            comment = request.POST.get('comment', 'NA')
+
+            item = StorageItem.objects.using('ig').get(file_id=file_id)
+            path = item.path
+            item.delete(using='ig')
+
+            new_item = StorageItem(file_id=new_file_id, comment=comment, path=path)
+            new_item.save(using='ig')
+
             form = StorageItemForm()
         else:
             form = StorageItemForm(request.POST)
@@ -23,13 +36,13 @@ def view(request):
                 if 'btn_create' in request.POST:
                     try:
                         StorageItem.objects.using('ig').get(file_id=data['file_id'])
-                        return render(request, 'igstorage/show_storage_items.html', dictionary={'form': form, 'result': 'Already exists'})
+                        return render(request, 'igstorage/show_storage_items.html', dictionary={'form': form, 'status': 'Already exists'})
                     except StorageItem.DoesNotExist:
                         item = StorageItem(file_id=data['file_id'], comment=data['comment'], path=data['path'])
                         item.save(using='ig')
 
                     items = StorageItem.objects.using('ig').all()
-                    return render(request, 'igstorage/show_storage_items.html', dictionary={'form': form, 'result': 'OK', 'items': items})
+                    return render(request, 'igstorage/show_storage_items.html', dictionary={'form': form, 'status': 'OK', 'items': items})
     else:
         form = StorageItemForm()  # An unbound form
 
