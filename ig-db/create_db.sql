ea286884e7c32340cf7ef3cd363b19e5e3958bd3
Pavel <zhmactep@gmail.com>
2013-11-11 16:28:58 +0300

    Update create_db.sql

--- ig-db/create_db.sql
+++ ig-db/create_db.sql
@@ -8,24 +8,3 @@ create table igbackend_tasks (
 	status ENUM('ok', 'failed', 'in progress'),
 	PRIMARY KEY (id)
 );
-
-
-#### after syncdb:
-
-drop table django.igtools_train;
-drop table django.igtools_predict;
-drop table django.igtools_simplecluster;
-drop table django.igstorage_storageitem;
-drop table ig.auth_user_groups;
-drop table ig.auth_group_permissions;
-drop table ig.auth_group;
-drop table ig.auth_user_user_permissions;
-drop table ig.auth_permission;
-drop table ig.django_admin_log;
-drop table ig.django_content_type;
-drop table ig.django_session;
-drop table ig.django_site;
-drop table ig.messages_message;
-drop table ig.messages_dialog;
-drop table ig.registration_registrationprofile;
-drop table ig.auth_user;
