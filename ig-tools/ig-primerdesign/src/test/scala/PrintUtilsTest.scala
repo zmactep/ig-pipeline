57780dccc40b651cde69bb0d8e0ed27d94f98cfb
Konstantin Fedorov <k.a.feodorov@gmail.com>
2014-03-07 09:49:29 +0400

    test samples added

--- ig-tools/ig-primerdesign/src/test/scala/PrintUtilsTest.scala
+++ ig-tools/ig-primerdesign/src/test/scala/PrintUtilsTest.scala
@@ -28,5 +28,18 @@ class PrintUtilsTest extends FunSpec {
       val indicies = DnaUtils.findOverlaps(nucl, 20, 40)
       PrintUtils.printOverlaps(nucl, indicies, 20)
     }
+
+    it ("should work with Pasha's dataset :)") {
+      import ru.biocad.ig.primer.ConversionUtils._
+      import ru.biocad.ig.primer.DnaUtils._
+      for (protein <- List("DIQMTQSPSSLSASVGDRVTITCKASQSVSSDVGWYQQKPGKAPKLLIYSGSNRYSGVPSRFSGSGSGTDFTLTISSLQPEDFATYYCQQDYYSPWTFGQGTKVEIK","EVQLVQSGAEVKKPGSSVKVSCKASGYTFTNYVINWVRQAPGQGLEWIGYNDPYNDVSKYNEKFKGRATITSDKSTSTAYMELSSLRSEDTAVYYCAKEGGGKYVYAMDSWGQGTTVTVSS")) {
+        val triq = ProteinTriequence(protein)
+        val ds = new SimpleProbabilityDecisionStrategy(Map("A" -> 5, "C" -> 5, "G" -> 5, "T" -> 5, "U" -> 5))
+        println(protein)
+        val nucl: Option[Sequence] = triq.sample(ds).map{s => println(s"$s straight"); println(s"${reverseComplementRNA(Option(s)).get} revcomp"); String2Sequence(s)}
+        val indicies = DnaUtils.findOverlaps(nucl, 20, 40)
+        PrintUtils.printOverlaps(nucl, indicies, 20)
+      }
+    }
   }
 }
